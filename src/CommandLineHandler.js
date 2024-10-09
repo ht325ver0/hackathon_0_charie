6861b0212d9135404257397b23982e66115eb8f1
Yutaro Tanaka <yutaro.tanaka.7r@stu.hosei.ac.jp>
2024-10-10 03:34:37 +0900

    Add files via upload

--- src/CommandLineHandler.js
+++ src/CommandLineHandler.js
@@ -1,4 +1,4 @@
 chrome.commands.onCommand.addListener((command) => {
-    if (command !== "open-popup") return;
-    chrome.tabs.create({ url: "https://developer.chrome.com" });
+  if (command !== "open-popup") return;
+  chrome.tabs.create({ url: "https://developer.chrome.com" });
 });
--- src/background.js
+++ src/background.js
@@ -1,5 +1,32 @@
 chrome.commands.onCommand.addListener((command) => {
-    if (command === "open-popup") {
-        chrome.action.openPopup(); // ポップアップを開く
+  const urlMap = {
+    'home': '',
+    'mail': 'https://mail.google.com/',
+    'tr' : 'https://translate.google.co.jp/',
+    'map' : 'https://maps.google.com/',
+    'drive' : 'https://drive.google.com/',
+    'docs' : 'https://docs.google.com/',
+    'sheets' : 'https://spreadsheets.google.com/',
+    'slides' : 'https://slides.google.com/',
+    'keeps' : 'https://slides.google.com/',
+    'photos' : 'https://calendar.google.com/',
+    'meet' : 'https://meet.google.com/',
+    'yt' : 'https://www.youtube.com/',
+    'gemini' : 'https://gemini.google.com/',
+    'dino' : 'chrome://dino/',
+
+  };
+
+  if(command === "open-popup") {
+    chrome.action.openPopup();
+  }
+  else if(isTerminalMode) {
+    const targetUrl = urlMap[command];
+    if(targetUrl) {
+      chrome.tabs.create({ url: targetUrl });
     }
+    else {
+      console.warn(`Unsupported command: ${command}`);
+    }
+  }
 });
