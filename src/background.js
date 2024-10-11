dd7920ac00289117791aca2bfeacc4fee41b87e1
田中優太朗 <yutaro.tanaka.7r@stu.hosei.ac.jp>
2024-10-12 06:33:25 +0900

    ポップアップデザインの変更、コマンドの頭に"/"を追加

--- src/background.js
+++ src/background.js
@@ -1,21 +1,21 @@
 chrome.commands.onCommand.addListener((command) => {
   const urlMap = {
-    'home': '',
-    'mail': 'https://mail.google.com/',
-    'tr' : 'https://translate.google.co.jp/',
-    'map' : 'https://maps.google.com/',
-    'drive' : 'https://drive.google.com/',
-    'docs' : 'https://docs.google.com/',
-    'sheets' : 'https://spreadsheets.google.com/',
-    'slides' : 'https://slides.google.com/',
-    'keeps' : 'https://slides.google.com/',
-    'photos' : 'https://calendar.google.com/',
-    'meet' : 'https://meet.google.com/',
-    'yt' : 'https://www.youtube.com/',
-    'gemini' : 'https://gemini.google.com/',
-    'dino' : 'chrome://dino/',
-
+    '/home': '',
+    '/mail': 'https://mail.google.com/',
+    '/tr' : 'https://translate.google.co.jp/',
+    '/map' : 'https://maps.google.com/',
+    '/drive' : 'https://drive.google.com/',
+    '/docs' : 'https://docs.google.com/',
+    '/sheets' : 'https://spreadsheets.google.com/',
+    '/slides' : 'https://slides.google.com/',
+    '/keeps' : 'https://slides.google.com/',
+    '/photos' : 'https://calendar.google.com/',
+    '/meet' : 'https://meet.google.com/',
+    '/yt' : 'https://www.youtube.com/',
+    '/gemini' : 'https://gemini.google.com/',
+    '/dino' : 'chrome://dino/',
   };
+  // ユーザーがカスタムできるショートカットも追加したい...
 
   if(command === "open-popup") {
     chrome.action.openPopup();
