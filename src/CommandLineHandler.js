2fd2f9dd19444a6584f4b9faa72210c11dd6b566
ht325ver0 <sou10aka6@doomo.ne.jp>
2024-10-12 01:17:49 +0900

    10/12 とりま実装完了

--- src/CommandLineHandler.js
+++ src/CommandLineHandler.js
@@ -1,4 +1,30 @@
-chrome.commands.onCommand.addListener((command) => {
-    if (command !== "open-popup") return;
-    chrome.tabs.create({ url: "https://developer.chrome.com" });
-});
+export function isValidInput(input){
+    const urlMap = {
+        'home': 'https://www.google.com',
+        'mail': 'https://mail.google.com/',
+        'tr' : 'https://translate.google.co.jp/',
+        'map' : 'https://maps.google.com/',
+        'drive' : 'https://drive.google.com/',
+        'docs' : 'https://docs.google.com/',
+        'sheets' : 'https://spreadsheets.google.com/',
+        'slides' : 'https://slides.google.com/',
+        'keeps' : 'https://slides.google.com/',
+        'photos' : 'https://calendar.google.com/',
+        'meet' : 'https://meet.google.com/',
+        'yt' : 'https://www.youtube.com/',
+        'gemini' : 'https://gemini.google.com/',
+        'dino' : 'chrome://dino/',
+    };
+    Object.entries(urlMap).forEach(([key, value]) => {
+      if(input == key) {
+
+        const targetUrl = value;
+        if(targetUrl) {
+          chrome.tabs.create({ url: targetUrl });
+        }
+        else {
+          console.warn(`Unsupported command: ${command}`);
+        }
+      }
+    });
+}
--- src/background.js
+++ src/background.js
@@ -1,32 +1,9 @@
 chrome.commands.onCommand.addListener((command) => {
-    const urlMap = {
-      'home': '',
-      'mail': 'https://mail.google.com/',
-      'tr' : 'https://translate.google.co.jp/',
-      'map' : 'https://maps.google.com/',
-      'drive' : 'https://drive.google.com/',
-      'docs' : 'https://docs.google.com/',
-      'sheets' : 'https://spreadsheets.google.com/',
-      'slides' : 'https://slides.google.com/',
-      'keeps' : 'https://slides.google.com/',
-      'photos' : 'https://calendar.google.com/',
-      'meet' : 'https://meet.google.com/',
-      'yt' : 'https://www.youtube.com/',
-      'gemini' : 'https://gemini.google.com/',
-      'dino' : 'chrome://dino/',
-  
-    };
+    
   
     if(command === "open-popup") {
       chrome.action.openPopup();
+      isTerminalMode = true;
     }
-    else if(isTerminalMode) {
-      const targetUrl = urlMap[command];
-      if(targetUrl) {
-        chrome.tabs.create({ url: targetUrl });
-      }
-      else {
-        console.warn(`Unsupported command: ${command}`);
-      }
-    }
+    
   });
